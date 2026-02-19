c844d48bc30ffac214aeb337457536a3a15df7f9
kri8tinam <chrissy8893@gmail.com>
2026-02-19 23:46:38 +0100

    Fixed batches and OOM issues

--- run_radiomics.py
+++ run_radiomics.py
@@ -1,10 +1,10 @@
 import os, sys
 import numpy as np
 import pandas as pd
-from radiomics_pipeline import feature_extract, save_results
+from radiomics_pipeline import feature_extract, append_df_to_csv
 
 def main():
-    data = pd.read_csv("/projects/net_contrast_classification/contrast_phase/dataset-registration-artinet.csv")
+    data = pd.read_csv("/projects/net_contrast_classification/contrast_phase/cleaned_data.csv")
     folder_path = "/mnt/rhea/data_private/IRBd23-231/GEPNETs/ARTINET"
     output_path = "/projects/net_contrast_classification/contrast_phase/features"
     os.makedirs(output_path, exist_ok=True)
@@ -14,18 +14,6 @@ def main():
 
     num_batches = 4
     idx_splits = np.array_split(np.arange(len(files)), num_batches)
-
-    # # batch_id = None
-    # if len(sys.argv) > 1:
-    #     batch_id = int(sys.argv[1])  # treats the 2nd arg as the batch index
-    # elif "SLURM_ARRAY_TASK_ID" in os.environ:
-    #     batch_id = int(os.environ["SLURM_ARRAY_TASK_ID"])
-    # else:
-    #     batch_id = 0
-
-    # if batch_id >= num_batches:
-    #     print(f"Batch {batch_id} out of range, exiting.")
-    #     return
 
     if len(sys.argv) != 2:
         print("Usage: python run_radiomics.py <batch_index>")
@@ -42,10 +30,20 @@ def main():
     batch_files = [files[i] for i in batch_indices]
     batch_phases = [phases[i] for i in batch_indices]
 
-    df = feature_extract(batch_files, batch_phases, folder_path)
     output_csv_path = os.path.join(output_path, f"features_batch_{batch_id}.csv")
-    save_results(df, output_csv_path)
-    print(f"Job {batch_id} done. Processed {len(df)} files. Saved to {output_csv_path}")
+    wrote_header = False
+    processed_images, total_rows = 0, 0 
+
+    for file_path, phase in zip(batch_files, batch_phases):
+        df_img = feature_extract(file_path, phase, folder_path)
+        wrote_header, processed_images, total_rows = append_df_to_csv(
+            df_img, output_csv_path, wrote_header, processed_images, total_rows)
+
+
+    print(
+        f"Job {batch_id} done. Processed {processed_images} images, "
+        f"wrote {total_rows} feature rows to {output_csv_path}"
+    )
 
 
 if __name__ == "__main__":
