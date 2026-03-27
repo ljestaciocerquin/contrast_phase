59a294e90ed8b5618024d47c4e797c86788e48c4
kri8tinam <chrissy8893@gmail.com>
2026-03-27 19:50:52 +0100

    Changed the memory and GPU request

--- Time_estim/time_estim.sh
+++ Time_estim/time_estim.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=rtx2080ti
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:2
-#SBATCH --mem=20G
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
 #SBATCH --time=24:00:00
 #SBATCH --qos=rtx_qos
 #SBATCH --output=/projects/net_contrast_classification/jobs/time_estim/time_estim_%A.out
