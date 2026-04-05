0d8127c23561da2190ed70f244c1fcd9d8846a0c
kri8tinam <chrissy8893@gmail.com>
2026-04-05 15:51:07 +0200

    Adidng DL module for contrast and phase timing classification

---
+++ DeepLClassifiers/contrast_3d.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=contrast_3d
+#SBATCH --partition=rtx2080ti
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --time=24:00:00
+#SBATCH --qos=rtx_qos
+#SBATCH --output=/projects/net_contrast_classification/jobs/deep_class/contrast_3d_%A.out
+#SBATCH --error=/projects/net_contrast_classification/jobs/deep_class/contrast_3d_%A.err
+
+source /home/k.minkova/miniconda3/etc/profile.d/conda.sh
+conda activate class
+
+cd /projects/net_contrast_classification/contrast_phase
+
+python DeepLClassifiers/contrast_3d_cnn.py
