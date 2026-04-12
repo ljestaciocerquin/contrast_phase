0767a7898bd23574893d38cbd146f2f6bfcb1796
kri8tinam <chrissy8893@gmail.com>
2026-04-12 23:42:09 +0200

    Ensured GPU usage

--- DeepLClassifiers/contrast_3d.sh
+++ DeepLClassifiers/contrast_3d.sh
@@ -4,12 +4,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=3-00:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem=40G
-#SBATCH --time=24:00:00
 #SBATCH --qos=rtx_qos
-#SBATCH --output=/projects/net_contrast_classification/jobs/deep_class/contrast_3d_%A.out
-#SBATCH --error=/projects/net_contrast_classification/jobs/deep_class/contrast_3d_%A.err
+#SBATCH --array=0-1
+#SBATCH --output=/projects/net_contrast_classification/jobs/deep_class/contrast_3d_%A_%a.out
+#SBATCH --error=/projects/net_contrast_classification/jobs/deep_class/contrast_3d_%A_%a.err
+
 
 source /home/k.minkova/miniconda3/etc/profile.d/conda.sh
 conda activate class
