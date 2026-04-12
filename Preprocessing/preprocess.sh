6d4045d36b2b59d720b7ad776e6ffbde19aad824
kri8tinam <chrissy8893@gmail.com>
2026-04-12 23:45:47 +0200

    Parallelized the preprocessing

--- Preprocessing/preprocess.sh
+++ Preprocessing/preprocess.sh
@@ -4,12 +4,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
-#SBATCH --mem=40G
-#SBATCH --time=24:00:00
+#SBATCH --mem=16G
+#SBATCH --time=3-00:00:00
 #SBATCH --qos=rtx_qos
-#SBATCH --output=/projects/net_contrast_classification/jobs/preprocessing/preprocess_%A.out
-#SBATCH --error=/projects/net_contrast_classification/jobs/preprocessing/preprocess_%A.err
+#SBATCH --array=0-29
+#SBATCH --output=/projects/net_contrast_classification/jobs/preprocessing/trial1/preprocess_%A_%a.out
+#SBATCH --error=/projects/net_contrast_classification/jobs/preprocessing/trial1/preprocess_%A_%a.err
+
 
 source /home/k.minkova/miniconda3/etc/profile.d/conda.sh
 conda activate class
